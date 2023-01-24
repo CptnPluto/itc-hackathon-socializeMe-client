bd7b05a0a5cb9e44333e67f5bf1ec7b64a4a6a25
CptnPluto <cptnpluto@gmail.com>
2023-01-24 20:11:59 +0200

    Fixed axios get request.

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 function App() {
     const clickMe = async () => {
-        const res = await axios.get(`${process.env.REACT_APP_SERVER_URL}/`);
+        const res = await axios.get(`${process.env.REACT_APP_SERVER_URL}/hello`);
         console.log(res);
     };
 
