4a0f14759b476ee8ee5f5f2a5d7dcc69ed6374ac
sarahitsko <112071488+sarahitsko@users.noreply.github.com>
2023-01-25 21:52:45 +0200

    Landing page styling

--- src/pages/LandingPage/LandingPage.js
+++ src/pages/LandingPage/LandingPage.js
@@ -2,44 +2,52 @@ import axios from "axios";
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import "./LandingPage.css";
+import skyline from "../../images/skyline.avif";
+import location from "../../images/location.svg";
+import activities from "../../images/activities.svg";
+import newEx from "../../images/newEx.svg";
 
 export default function LandingPage() {
-    let navigate = useNavigate();
-    const handleNavigate = () => {
-        navigate("/chooseLocation");
-    };
+  let navigate = useNavigate();
+  const handleNavigate = () => {
+    navigate("/chooseLocation");
+  };
+  return (
+    <>
+      <div className="container">
+        <div className="bgImg">
+          <img
+            className="imgCover"
+            src={skyline}
+            alt="Front "
+            width="200"
+            height="100"
+          />
+        </div>
+      </div>
+      <h3 className="welcome">
+        Welcome to SocializeMe - You Dont Knoe What You Dont Know...But We Do
+      </h3>
+      <div className="svg">
+        <div className="homeDiv">
+          {" "}
+          <img className="location" src={location} alt="location" />{" "}
+          <span className="homespan">Coose Your Current Location</span>
+        </div>
+        <div className="homeDiv">
+          {" "}
+          <img className="activities" src={activities} alt="activities" />{" "}
+          <span className="homespan">Choose Your Favorite Activities</span>
+        </div>
+        <div className="homeDiv">
+          <img className="newEx" src={newEx} alt="newEx" />{" "}
+          <span className="homespan">Discover New Cultural Experiences</span>
+        </div>
+      </div>
 
-    // const doTest = async () => {
-    //     console.log("testing");
-    //     try {
-    //         console.log("trying model");
-    //         const res = await axios.get(
-    //             "http://localhost:8080/events/ds/1/NYC"
-    //         );
-    //         console.log(res);
-    //     } catch (err) {
-    //         console.log(err);
-    //     }
-    // };
-
-    return (
-        <>
-            <div className="head">
-                <h2 className="headLine">SocializeMe</h2>
-            </div>
-            <div className="aboutText">
-                "SocializeMe" help you find similar events in your current city
-                by comparing it with your home city. Simply enter your home and
-                current city and discover new cultural experiences.
-            </div>
-            <div className="startButton">
-                <button onClick={handleNavigate}>Start</button>
-            </div>
-            {/* <div className="test">
-                <button onClick={doTest} type="button">
-                    Test
-                </button>
-            </div> */}
-        </>
-    );
+      <div className="startButton">
+        <button onClick={handleNavigate}>Start</button>
+      </div>
+    </>
+  );
 }
