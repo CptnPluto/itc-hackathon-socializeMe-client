4b569341ad5f9c5ecfba2762c560a7b93782c669
David <susuvnkl6@gmail.com>
2023-01-25 15:23:44 +0200

    hide menue on click added

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -34,15 +34,6 @@ export default function NavBar() {
     <>
       <div className={`${isNavbarVisible ? "fadeIn" : "fadeOut"}`}>
         <div id="navBarContainer">
-<<<<<<< HEAD
-          <div id="navBar" className="me-auto" defaultactivekey="/">
-            Navigation Dev
-            <button onClick={() => navigate("/chooseCategory")}>Choose Category Page</button>
-            <button onClick={() => navigate("/chooseEvent")}>Choose Event Page</button>
-            <button onClick={() => navigate("/")}>Landing Page</button>
-            <button onClick={() => navigate("/LocationPage")}>Choose Locations </button>
-            <button onClick={() => navigate("/results")}>Results Page</button>
-=======
           <img id="logoImg" onClick={() => navigate("/")} src={Logo} alt="Logo"></img>
           <div id="navBar" className="me-auto" defaultActiveKey="/"></div>
           <div id="humburgerMenu">
@@ -55,15 +46,49 @@ export default function NavBar() {
                   <div className="bottom"></div>
                 </label>
                 <nav className="menu1">
-                  <button onClick={() => navigate("/chooseCategory")}>Choose Category Page</button>
-                  <button onClick={() => navigate("/chooseEvent")}>Choose Event Page</button>
-                  <button onClick={() => navigate("/")}>Landing Page</button>
-                  <button onClick={() => navigate("/chooseLocation")}>Choose Locations </button>
-                  <button onClick={() => navigate("/results")}>Results Page</button>
+                  <button
+                    onClick={() => {
+                      navigate("/chooseCategory");
+                      setChecked(!checked);
+                    }}
+                  >
+                    Choose Category Page
+                  </button>
+                  <button
+                    onClick={() => {
+                      navigate("/chooseEvent");
+                      setChecked(!checked);
+                    }}
+                  >
+                    Choose Event Page
+                  </button>
+                  <button
+                    onClick={() => {
+                      navigate("/");
+                      setChecked(!checked);
+                    }}
+                  >
+                    Landing Page
+                  </button>
+                  <button
+                    onClick={() => {
+                      navigate("/chooseLocation");
+                      setChecked(!checked);
+                    }}
+                  >
+                    Choose Locations{" "}
+                  </button>
+                  <button
+                    onClick={() => {
+                      navigate("/results");
+                      setChecked(!checked);
+                    }}
+                  >
+                    Results Page
+                  </button>
                 </nav>
               </nav>
             </section>
->>>>>>> navbar
           </div>
         </div>
       </div>
