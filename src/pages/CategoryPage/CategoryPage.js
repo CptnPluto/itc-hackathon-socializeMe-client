f50facd71a6ea495f17a21f843e95a86b374f7b4
Sheva Vulakh <Sheva@HON-2016532.local>
2023-01-25 13:38:22 +0200

    categories

--- src/pages/CategoryPage/CategoryPage.js
+++ src/pages/CategoryPage/CategoryPage.js
@@ -1,15 +1,34 @@
 import React from 'react'
 import './CategoryPage.css'
 function CategoryPage() {
-
+const handleFood =(e)=>{
+  e.preventDefault()
+  console.log("food")
+}
+const handleMusic =(e)=>{
+  e.preventDefault()
+  console.log("music")
+}
+const handleMuseums =(e)=>{
+  e.preventDefault()
+  console.log("museums");
+}
+const handleOutDoors=(e)=>{
+  e.preventDefault()
+  console.log("outDoors");
+}
+const handleNext=(e)=>{
+  e.preventDefault()
+  console.log("next");
+}
   return (
     <div className="categoryPage">
-      <form>
+      <form onSubmit={handleNext}>
       <div className="categoryPageButtons">
-        <button className="btn btn-1 btn-sep icon-info">Food</button>
-        <button className="btn btn-2 btn-sep icon-cart">Music</button>
-        <button className="btn btn-3 btn-sep icon-heart">Museums</button>
-        <button className="btn btn-4 btn-sep icon-send">Outdoors</button>
+        <button onClick={handleFood} className="btn btn-1 btn-sep icon-info">Food</button>
+        <button onClick={handleMusic}className="btn btn-2 btn-sep icon-cart">Music</button>
+        <button onClick={handleMuseums}className="btn btn-3 btn-sep icon-heart">Museums</button>
+        <button onClick={handleOutDoors}className="btn btn-4 btn-sep icon-send">Outdoors</button>
         </div>
         <button>Next</button>
       </form>
