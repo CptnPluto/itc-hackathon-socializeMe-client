78a1a14ac691bd240ee760911d09542990cc81f6
CptnPluto <cptnpluto@gmail.com>
2023-01-26 00:19:28 +0200

    Added function on results page to re-fetch results based on text input from user.

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import ChooseEventPage from "./pages/ChooseEventPage/ChooseEventPage";
 import LandingPage from "./pages/LandingPage/LandingPage";
 import LocationPage from "./pages/LocationsPage/LocationPage";
 import LocationPageTo from "./pages/LocationsPage/LocationPageTo";
-import ResultsPAge from "./pages/ResultsPage/ResultsPAge";
+import ResultsPAge from "./pages/ResultsPage/ResultsPage";
 import "./App.css";
 import ResultsContextProvider from "./context/ResultsContext";
 
