efad8f203f851760c67143440720cf03d222ffe0
David <susuvnkl6@gmail.com>
2023-01-25 15:10:11 +0200

    navbar design Logo added humburger menue added

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,13 +1,14 @@
 import React, { useContext, useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import "./NavBar.css";
+import Logo from "../../SVG/Logo.svg";
 
 export default function NavBar() {
   const navigate = useNavigate();
   const [isNavbarVisible, setIsNavbarVisible] = useState(true);
   const [prevScrollpos, setPrevScrollpos] = useState(window.pageYOffset);
-  //   const [checked, setChecked] = useState(false);
-  // const [showNav, setShowNav] = useState(true);
+  const [checked, setChecked] = useState(false);
+  //   const [showNav, setShowNav] = useState(true);
 
   useEffect(() => {
     const handleScroll = () => {
@@ -33,13 +34,26 @@ export default function NavBar() {
     <>
       <div className={`${isNavbarVisible ? "fadeIn" : "fadeOut"}`}>
         <div id="navBarContainer">
-          <div id="navBar" className="me-auto" defaultActiveKey="/">
-            Navigation Dev
-            <button onClick={() => navigate("/chooseCategory")}>Choose Category Page</button>
-            <button onClick={() => navigate("/chooseEvent")}>Choose Event Page</button>
-            <button onClick={() => navigate("/")}>Landing Page</button>
-            <button onClick={() => navigate("/chooseLocation")}>Choose Locations </button>
-            <button onClick={() => navigate("/results")}>Results Page</button>
+          <img id="logoImg" onClick={() => navigate("/")} src={Logo} alt="Logo"></img>
+          <div id="navBar" className="me-auto" defaultActiveKey="/"></div>
+          <div id="humburgerMenu">
+            <section className="p-menu1">
+              <nav id="navbar" className="navigation" role="navigation">
+                <input id="toggle1" type="checkbox" checked={checked} />
+                <label onClick={() => setChecked(!checked)} className="hamburger1" htmlFor="toggle1">
+                  <div className="top"></div>
+                  <div className="meat"></div>
+                  <div className="bottom"></div>
+                </label>
+                <nav className="menu1">
+                  <button onClick={() => navigate("/chooseCategory")}>Choose Category Page</button>
+                  <button onClick={() => navigate("/chooseEvent")}>Choose Event Page</button>
+                  <button onClick={() => navigate("/")}>Landing Page</button>
+                  <button onClick={() => navigate("/chooseLocation")}>Choose Locations </button>
+                  <button onClick={() => navigate("/results")}>Results Page</button>
+                </nav>
+              </nav>
+            </section>
           </div>
         </div>
       </div>
