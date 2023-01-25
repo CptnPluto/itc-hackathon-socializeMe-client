57ae5ed41b4e772795ac46fdd9b239f66d0e24a8
CptnPluto <cptnpluto@gmail.com>
2023-01-25 23:51:20 +0200

    Starting to modify ResultsPage

--- src/pages/ResultsPage/ResultsPAge.js
+++ src/pages/ResultsPage/ResultsPAge.js
@@ -1,5 +1,12 @@
 import React from "react";
 
 export default function ResultsPAge() {
-  return <div>ResultsPAge</div>;
+    return (
+        <>
+            <div>ResultsPAge</div>
+            <div className="text-description">
+                
+            </div>
+        </>
+    );
 }
