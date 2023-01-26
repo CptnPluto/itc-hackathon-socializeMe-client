6dfd5515b722fe45478a703fbf5c042b829536f5
CptnPluto <cptnpluto@gmail.com>
2023-01-26 08:58:43 +0200

    Changed ChooseEventPage "next" to nav to results.

--- src/pages/ChooseEventPage/ChooseEventPage.js
+++ src/pages/ChooseEventPage/ChooseEventPage.js
@@ -1,19 +1,30 @@
 import React from "react";
 import Button from "../../components/Button/Button";
 import "./ChooseEventPage.css";
+import { useNavigate } from "react-router-dom";
 
 export default function ChooseEventPage() {
-  const buttonHandel = () => {
-    console.log(`button`);
-  };
-  return (
-    <>
-      <div id="topButton">
-        <Button buttonText={"prev"} callback={buttonHandel} width={`${16}%`} />
-      </div>
-      <div id="bottomButton">
-        <Button buttonText={"NEXT"} callback={buttonHandel} width={`${84}%`} />
-      </div>
-    </>
-  );
+    const navigate = useNavigate();
+    const buttonHandel = () => {
+        console.log(`button`);
+        navigate("/results");
+    };
+    return (
+        <>
+            <div id="topButton">
+                <Button
+                    buttonText={"prev"}
+                    callback={buttonHandel}
+                    width={`${16}%`}
+                />
+            </div>
+            <div id="bottomButton">
+                <Button
+                    buttonText={"NEXT"}
+                    callback={buttonHandel}
+                    width={`${84}%`}
+                />
+            </div>
+        </>
+    );
 }
