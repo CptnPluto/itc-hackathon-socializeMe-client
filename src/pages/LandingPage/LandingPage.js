b8ceacbd9e53fde0498a8d7f23e5795a2aded1cf
sarahitsko <112071488+sarahitsko@users.noreply.github.com>
2023-01-26 10:58:29 +0200

    about final design

--- src/pages/LandingPage/LandingPage.js
+++ src/pages/LandingPage/LandingPage.js
@@ -6,115 +6,91 @@ import skyline from "../../images/skyline.avif";
 import location from "../../images/location.svg";
 import activities from "../../images/activities.svg";
 import newEx from "../../images/newEx.svg";
+import ButtonCustom from "../../components/Button/ButtonCustom";
+import Typewriter from "react-typewriter";
 
 import "../../App.css";
 
 export default function LandingPage() {
-    const [transition, setTransition] = useState(false);
-    const [loadIn, setLoadIn] = useState(false);
-    let navigate = useNavigate();
+  let navigate = useNavigate();
+  const handleNavigate = () => {
+    setLoadIn(false);
+    setTimeout(() => {
+      navigate("/chooseLocation");
+    }, 500);
+  };
 
-    const handleNavigate = () => {
-        setLoadIn(false);
-        setTimeout(() => {
-            navigate("/chooseLocation");
-        }, 500);
-    };
+  //This isn't working: no cookies found. Need to fix.
+  const handleUserEvents = async () => {
+    console.log("user events");
+    const res = await axios.get("http://localhost:8080/users/usersEvents", {
+      withCredentials: true,
+    });
+    console.log("Users Events: ", res.data);
+  };
 
-    const handleSignup = () => {
-        console.log("sign up");
-        navigate("/signup");
-    };
+  useEffect(() => {
+    setLoadIn(true);
+  }, []);
 
-    const handleLogin = () => {
-        console.log("login");
-        navigate("/login");
-    };
-
-    //This isn't working: no cookies found. Need to fix.
-    const handleUserEvents = async () => {
-        console.log("user events");
-        const res = await axios.get("http://localhost:8080/users/usersEvents", {
-            withCredentials: true,
-        });
-        console.log("Users Events: ", res.data);
-    };
-
-    useEffect(() => {
-        setLoadIn(true);
-    }, []);
-
-    return (
-        <div
-            className={
-                loadIn ? "page_container load_in" : "page_container load_out"
-            }
-        >
-            <div className="container">
-                <div className="bgImg">
-                    <img
-                        className="imgCover"
-                        src={skyline}
-                        alt="Front "
-                        width="200"
-                        height="100"
-                    />
-                </div>
-            </div>
-            <h3 className="welcome">
-                Welcome to SocializeMe - You Dont Knoe What You Dont Know...But
-                We Do
-            </h3>
-            <div className="svg">
-                <div className="homeDiv">
-                    {" "}
-                    <img
-                        className="location"
-                        src={location}
-                        alt="location"
-                    />{" "}
-                    <span className="homespan">
-                        Coose Your Current Location
-                    </span>
-                </div>
-                <div className="homeDiv">
-                    {" "}
-                    <img
-                        className="activities"
-                        src={activities}
-                        alt="activities"
-                    />{" "}
-                    <span className="homespan">
-                        Choose Your Favorite Activities
-                    </span>
-                </div>
-                <div className="homeDiv">
-                    <img className="newEx" src={newEx} alt="newEx" />{" "}
-                    <span className="homespan">
-                        Discover New Cultural Experiences
-                    </span>
-                </div>
-            </div>
-
-            <div className="startButton">
-                <button onClick={handleNavigate}>Start</button>
-            </div>
-
-            <div className="testDiv">
-                <button type="button" classname="signup" onClick={handleSignup}>
-                    Sign Up!
-                </button>
-                <button type="button" classname="login" onClick={handleLogin}>
-                    Login!
-                </button>
-                <button
-                    type="button"
-                    classname="userEvents"
-                    onClick={handleUserEvents}
-                >
-                    User Events!
-                </button>
-            </div>
+  return (
+    <div
+      className={loadIn ? "page_container load_in" : "page_container load_out"}
+    >
+      <div className="container">
+        <div className="bgImg">
+          <img
+            className="imgCover"
+            src={skyline}
+            alt="Front "
+            width="200"
+            height="100"
+          />
         </div>
-    );
+      </div>
+      <div className="welcome">
+        <h3>Welcome to SocializeMe</h3>
+        <h4>You Dont Know What You Dont Know...</h4>
+        <h3>But we do</h3>
+      </div>
+      <Typewriter
+        className="typewriter - effect"
+        options={{
+          strings: ["...But We Do"],
+          autoStart: true,
+          delay: 40,
+        }}
+      />
+
+      <div className="svg">
+        <div className="homeDiv">
+          {" "}
+          <img className="location" src={location} alt="location" />{" "}
+          <span className="homespan">Coose Your Current Location</span>
+        </div>
+        <div className="homeDiv">
+          {" "}
+          <img className="activities" src={activities} alt="activities" />{" "}
+          <span className="homespan">Choose Your Favorite Activities</span>
+        </div>
+        <div className="homeDiv">
+          <img className="newEx" src={newEx} alt="newEx" />{" "}
+          <span className="homespan">Discover New Cultural Experiences</span>
+        </div>
+      </div>
+      <ButtonCustom buttonText={"Let's Go"} callback={handleNavigate} />
+
+      <div className="testDiv">
+        <button type="button" classname="signup" onClick={handleSignup}>
+          Sign Up!
+        </button>
+        <button type="button" classname="login" onClick={handleLogin}>
+          Login!
+        </button>
+        <button type="button" classname="userEvents" onClick={handleUserEvents}>
+          User Events!
+        </button>
+      </div>
+    </div>
+  );
 }
