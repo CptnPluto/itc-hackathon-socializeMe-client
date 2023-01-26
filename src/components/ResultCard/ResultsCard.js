d24c901c6d062ffbc21fc15f1de610f9234dbb86
Gilad Shaul <giladshaul@hotmail.com>
2023-01-26 12:33:50 +0200

    Results List change2

--- src/components/ResultCard/ResultsCard.js
+++ src/components/ResultCard/ResultsCard.js
@@ -1,22 +1,36 @@
 import React from "react";
 import "./ResultsCard.css";
+import Logo from "../../SVG/categories/music-c.svg";
 
 export default function ResultsCard(result) {
   console.log("pppp", result);
   return (
     <div id="categories">
-      <div id="eventInfo-main">
-        <p>{result.name}</p>
-        <p>category: {result.category}</p>
-      </div>
-      <div id="eventInfo-sub">
-        <p>{result.time}</p>
-        <p>{result.date}</p>
-        <p>Genre: {result.genre}</p>
+      <div id="middle-card">
+        <div id="music-logo">
+          <img src={Logo}></img>
+        </div>
+        <div>
+          <div id="eventInfo-main">
+            <p>{result.name}</p>
+            <p>
+              <b>category:</b> {result.category}
+            </p>
+          </div>
+          <div id="eventInfo-sub">
+            <p>{result.date}</p>
+            <p>{result.time}</p>
+            <p>
+              <b>Genre:</b> {result.genre}
+            </p>
+          </div>
+        </div>
       </div>
       <div id="cardLink">
         <button>
-          <a href={result.url}>Go To Website</a>
+          <a href={result.url}>
+            <b>Go To Website</b>
+          </a>
         </button>
       </div>
     </div>
