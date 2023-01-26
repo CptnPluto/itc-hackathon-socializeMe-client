fe31e539cb49122ef27b0a752f7bb4365cf376f3
CptnPluto <cptnpluto@gmail.com>
2023-01-26 08:56:09 +0200

    Changed LocationForm to nav to chooseEvent

--- src/components/LocationForm/LocationForm.js
+++ src/components/LocationForm/LocationForm.js
@@ -1,39 +1,42 @@
-import React from 'react'
+import React from "react";
 import { useNavigate } from "react-router-dom";
-import ButtonCustom from '../Button/ButtonCustom';
-import './LocationForm.css'
+import ButtonCustom from "../Button/ButtonCustom";
+import "./LocationForm.css";
 
 function LocationForm() {
     const navigate = useNavigate();
 
     const handleClick = () => {
-        navigate("/chooseLocationTo")
-    }
+        navigate("/chooseEvent");
+    };
 
-  return (
+    return (
+        <div>
+            <div className="fromheader">
+                <h3 htmlFor="Location-From">
+                    Choose the city you are coming from:
+                </h3>
+            </div>
+            <div className="locationdiv">
+                <select
+                    className="cityfromform"
+                    name="citiesfrom"
+                    id="citiesfrom"
+                    form="citiesfrom"
+                >
+                    <option value="">Coming From...</option>
+                    <option value="LosAngeles">Los Angeles</option>
+                    <option value="Sacramento">Sacramento</option>
+                    <option value="NewYork">New York</option>
+                    <option value="Miami">Miami</option>
+                </select>
+            </div>
 
-<div>
-
-<div className='fromheader'>
-<h3 htmlFor="Location-From">Choose the city you are coming from:</h3>
-</div>
-<div className='locationdiv'>
-
-
-<select className='cityfromform' name="citiesfrom" id="citiesfrom" form="citiesfrom" >
-  <option value="">Coming From...</option>
-  <option value="LosAngeles">Los Angeles</option>
-  <option value="Sacramento">Sacramento</option>
-  <option value="NewYork">New York</option>
-  <option value="Miami">Miami</option>
-</select>
-</div>
-
-
-<div className='buttondiv'>
-<ButtonCustom buttonText={'Next'}callback={handleClick}/>
-</div></div>
-  )
+            <div className="buttondiv">
+                <ButtonCustom buttonText={"Next"} callback={handleClick} />
+            </div>
+        </div>
+    );
 }
 
-export default LocationForm
+export default LocationForm;
